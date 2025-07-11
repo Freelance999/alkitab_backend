27c13c623099b0664e2d103fc582713e96f4577e
ArifIlham0 <marifilham999@gmail.com>
2025-07-11 17:19:44 +0700

    refactor: clean code

--- alkitab_backend/views/song_book_view.py
+++ alkitab_backend/views/song_book_view.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.response import Response
 from rest_framework import status
-from alkitab_backend.models import ReadingPlan, SongBook
+from alkitab_backend.models import SongBook
 from alkitab_backend.serializers import SongBookSerializer
 
 @api_view(['POST'])
